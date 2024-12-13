ff39aab86a2a1ec2c75775eb76a1bd08562376df
heshamaref388 <heshamaref388@gmail.com>
2024-12-13 21:05:44 +0200

    Initial commit

--- src/Componants/Home/Home.jsx
+++ src/Componants/Home/Home.jsx
@@ -121,7 +121,12 @@ const DataDisplay = () => {
           </div>
 
           {/* Display filtered data */}
-          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <ul
+            className={`grid gap-6 ${darkMode ? "bg-gray-900" : "bg-gray-50"}`}
+            style={{
+              gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+            }}
+          >
             {filteredData.map((item) => (
               <li
                 key={item._id}
@@ -130,18 +135,18 @@ const DataDisplay = () => {
                   darkMode ? "bg-gray-800" : "bg-white"
                 }`}
               >
-                <div className="p-4 sm:p-4">
-                  <strong className="text-lg sm:text-xl font-semibold mb-2">
+                <div className="p-4">
+                  <h3 className="text-lg font-bold mb-2">
                     {item._source.name}
-                  </strong>
+                  </h3>
                   <p>
-                    <strong>{t("type")}</strong> {item._source.type}
+                    <strong>{t("type")}:</strong> {item._source.type}
                   </p>
                   <p>
-                    <strong>{t("country")}</strong> {item._source.country}
+                    <strong>{t("country")}:</strong> {item._source.country}
                   </p>
                   <p>
-                    <strong>{t("currency")}</strong> {item._source.currency}
+                    <strong>{t("currency")}:</strong> {item._source.currency}
                   </p>
                 </div>
               </li>
